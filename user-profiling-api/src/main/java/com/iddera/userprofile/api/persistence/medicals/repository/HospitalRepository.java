c668a517b57206bfb5f7011f758503d05fe413b7
Ajia  Oluwatobi <aoluwatobi@teamapt.com>
2021-05-04 18:38:28 +0100

    Added Hospital Endpoints

--- user-profiling-api/src/main/java/com/iddera/userprofile/api/persistence/medicals/repository/HospitalRepository.java
+++ user-profiling-api/src/main/java/com/iddera/userprofile/api/persistence/medicals/repository/HospitalRepository.java
@@ -2,8 +2,6 @@ package com.iddera.userprofile.api.persistence.medicals.repository;
 
 import com.iddera.userprofile.api.persistence.medicals.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 }
