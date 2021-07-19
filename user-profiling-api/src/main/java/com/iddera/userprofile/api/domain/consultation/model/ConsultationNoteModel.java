7c53d32e85dbbb178572c82bf4420849fd94fa42
sanmi <sanmiakinrele@gmail.com>
2021-07-19 19:57:26 +0100

    Added field level validation to consultation note model.

--- user-profiling-api/src/main/java/com/iddera/userprofile/api/domain/consultation/model/ConsultationNoteModel.java
+++ user-profiling-api/src/main/java/com/iddera/userprofile/api/domain/consultation/model/ConsultationNoteModel.java
@@ -3,14 +3,23 @@ package com.iddera.userprofile.api.domain.consultation.model;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 public class ConsultationNoteModel {
     private Long id;
+    @NotBlank(message = "History can not be null or empty.")
     private String history;
+    @NotBlank(message = "Examination can not be null or empty.")
     private String examination;
+    @NotBlank(message = "Investigation can not be null or empty.")
     private String investigation;
+    @NotBlank(message = "Diagnosis can not be null or empty.")
     private String diagnosis;
+    @NotBlank(message = "Plan can not be null or empty.")
     private String plan;
+    @NotNull(message = "Consultation Id can not be null")
     private Long consultationId;
 }
